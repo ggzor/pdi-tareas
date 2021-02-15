4ff6157d1669ab3357a340331bc0043ab44bc48a
ggzor <30713864+ggzor@users.noreply.github.com>
2021-02-15 01:14:35 -0600

    Agregando guardado de imágenes

--- imagenes/ImagenesIO.java
+++ imagenes/ImagenesIO.java
@@ -4,7 +4,10 @@ import utils.Either;
 
 import java.io.*;
 import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
+
+import javax.imageio.*;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.FileImageOutputStream;
 
 /**
  * Clase con funciones de utilería para obtener imágenes a partir de archivos
@@ -25,6 +28,29 @@ public class ImagenesIO {
       return Either.right(imagen);
     } catch (IOException ex) {
       return Either.left(ErrorImagen.ERROR_LECTURA);
+    }
+  }
+
+  public static Either<Void, Void> guardarImagen(BufferedImage imagen, File archivo) {
+    try {
+      if (archivo.getName().toLowerCase().endsWith(".jpg")) {
+        try (FileImageOutputStream os = new FileImageOutputStream(archivo)) {
+          JPEGImageWriteParam jpegParams = new JPEGImageWriteParam(null);
+          jpegParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+          jpegParams.setCompressionQuality(1f);
+
+          ImageWriter iw = ImageIO.getImageWritersByFormatName("jpg").next();
+          iw.setOutput(os);
+          iw.write(null, new IIOImage(imagen, null, null), jpegParams);
+
+          return Either.right(null);
+        }
+      } else {
+        ImageIO.write(imagen, "bmp", archivo);
+        return Either.right(null);
+      }
+    } catch(IOException ex) {
+      return Either.left(null);
     }
   }
 
